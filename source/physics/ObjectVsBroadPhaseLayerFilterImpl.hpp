fba30dec441af8adc5794368173c49724bf47aa6
bottledlactose <175543105+bottledlactose@users.noreply.github.com>
2024-11-23 13:18:49 +0100

    Split up physics manager objects

---
+++ source/physics/ObjectVsBroadPhaseLayerFilterImpl.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+/// Class that determines if an object layer can collide with a broadphase layer
+class ObjectVsBroadPhaseLayerFilterImpl : public JPH::ObjectVsBroadPhaseLayerFilter
+{
+public:
+	virtual bool ShouldCollide(JPH::ObjectLayer inLayer1, JPH::BroadPhaseLayer inLayer2) const override
+	{
+		switch (inLayer1)
+		{
+		case Layers::NON_MOVING:
+			return inLayer2 == BroadPhaseLayers::MOVING;
+		case Layers::MOVING:
+			return true;
+		default:
+			JPH_ASSERT(false);
+			return false;
+		}
+	}
+};
